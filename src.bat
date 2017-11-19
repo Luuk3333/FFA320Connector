4f20754d0143ed27717804b945f77bc177592a28
Till <tillvennefrohne@gmail.com>
2017-11-19 11:40:13 +0100

    Changed Xcode-Project to compile into project dir + mac.xpl, src.bat copies mac.xpl to the bin/64 folder

--- src.bat
+++ src.bat
@@ -1,2 +1,3 @@
 copy src_vs2013\FFA320Connector.cpp src\FFA320Connector.cpp
 copy src_vs2013\FFA320Connector.cpp src_xcode3.2\FFA320Connector.cpp
+copy src_xcode3.2\DerivedData\FFA320Connector\Build\Products\Release\mac.xpl bin\FFA320Connector\64\mac.xpl
---
+++ src_xcode3.2/DerivedData/FFA320Connector/Build/Intermediates/FFA320Connector.build/Debug/FFA320Connector.build/Objects-normal/x86_64/FFA320Connector.d
@@ -0,0 +1,6 @@
+dependencies: /Volumes/FFA320Connector/src_xcode3.2/FFA320Connector.cpp \
+  SDK/CHeaders/XPLM/XPLMDataAccess.h SDK/CHeaders/XPLM/XPLMDefs.h \
+  SDK/CHeaders/XPLM/XPLMPlugin.h SDK/CHeaders/XPLM/XPLMUtilities.h \
+  SDK/CHeaders/XPLM/XPLMProcessing.h SDK/CHeaders/XPLM/XPLMMenus.h \
+  SDK/CHeaders/XPLM/XPLMPlanes.h \
+  /Volumes/FFA320Connector/src_xcode3.2/SharedValue.h
